dbe153ef94d921d5d5a51ced839830bfba5377cc
Lieke22 <lieke22@gmail.com>
2014-01-02 20:16:09 +0100

    Significant word update

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,8 @@
 class Quote < ActiveRecord::Base
+  
+  def most_significant_word
+    quote.split.map { |quote| quote.gsub(/\W/, '') }.sort_by(&:length)[-1]
+  end
+
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -4,4 +4,20 @@ class QuoteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "Find most significant word" do
+  	assert_significant_word('anything', 'If the lessons of history teach us anything')
+	assert_significant_word('curiosity','The cure for boredom is curiosity')
+	assert_significant_word('discovery','The real voyage of discovery consists not in seeking new lands but seeing with new eyes')
+  end
+
+  test "Find most significant when equal length" do
+  	assert_significant_word('efgh', 'abcd efgh')
+  	assert_significant_word('abcd', 'efgh abcd')
+  end
+
+  def assert_significant_word(expectation, quote)
+  	assert_equal(expectation, Quote.new(:quote => quote).most_significant_word)
+  end
 end
